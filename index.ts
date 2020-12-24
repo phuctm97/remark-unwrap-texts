8e350a3c40a4ee8c5fcfb90c8bb028ee7bcdafa2
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-24 16:13:12 +0700

    ✨ Add plugin and typings

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Parent, Text } from "mdast";
+import visit from "unist-util-visit";
+
+/**
+ * Unwraps `text` nodes in Markdown.
+ *
+ * Is useful when publishing to platforms like DEV.to, Medium, Hashnode, etc.
+ * These platforms may not support text wraps and generate unexpected newlines.
+ */
+const plugin = () => (tree: Parent) => {
+  visit(tree, "text", (text: Text) => {
+    text.value = text.value.replace(/\n/g, " ");
+  });
+};
+
+export default plugin;
