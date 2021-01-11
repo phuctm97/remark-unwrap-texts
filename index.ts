5d5cb48fa232e3d28018f61080bdc95e33ac9428
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-11 19:00:43 +0700

    🐛 Fix types

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { Parent, Text } from "mdast";
+import { Plugin } from "unified";
+import { Text } from "mdast";
 import visit from "unist-util-visit";
 
 /**
@@ -7,7 +8,7 @@ import visit from "unist-util-visit";
  * Is useful when publishing to platforms like DEV.to, Medium, Hashnode, etc.
  * These platforms may not support text wraps and generate unexpected newlines.
  */
-const plugin = () => (tree: Parent) => {
+const plugin: Plugin = () => (tree) => {
   visit(tree, "text", (text: Text) => {
     text.value = text.value.replace(/\n/g, " ");
   });
